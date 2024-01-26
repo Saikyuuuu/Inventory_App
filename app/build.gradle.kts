685337da9bea92df0315e2b13bbb1860b08a5079
Saikyuuuu <mobilelegendsologamer@gmail.com>
2024-01-26 14:06:44 +0800

    Added dependencies. Deleted some unused xml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt") // Add this line for Kotlin annotation processing
 }
 
 android {
     namespace = "com.example.inventory_app"
     compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.example.inventory_app"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    // ... other configurations
 
     buildTypes {
         release {
@@ -36,7 +29,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -46,5 +38,20 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("com.airbnb.android:lottie:6.3.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 
+    implementation("com.google.accompanist:accompanist-pager:0.20.3")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.20.3")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0") // Added Hilt Navigation Compose
+
+    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
