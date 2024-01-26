97e84feeba8456f1ecefee2463deb445b22cad3f
Saikyuuuu <mobilelegendsologamer@gmail.com>
2024-01-26 14:18:15 +0800

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,21 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt") // Add this line for Kotlin annotation processing
 }
 
 android {
     namespace = "com.example.inventory_app"
     compileSdk = 34
 
-    // ... other configurations
+    defaultConfig {
+        applicationId = "com.example.inventory_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
     buildTypes {
         release {
@@ -29,29 +36,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//    Added dependencies
     implementation("com.airbnb.android:lottie:6.3.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
 
-    implementation("com.google.accompanist:accompanist-pager:0.20.3")
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.20.3")
 
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    implementation("com.google.dagger:hilt-android:2.38.1")
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    implementation("androidx.hilt:hilt-work:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0") // Added Hilt Navigation Compose
-
-    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
