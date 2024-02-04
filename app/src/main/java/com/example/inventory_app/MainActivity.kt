8dbe2b88a06eeb58c8d2bb69fdb5e504d0d14552
Saikyuuuu <mobilelegendsologamer@gmail.com>
2024-02-04 20:52:57 +0800

    Added log in, register feature

--- app/src/main/java/com/example/inventory_app/MainActivity.kt
+++ app/src/main/java/com/example/inventory_app/MainActivity.kt
@@ -5,23 +5,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.inventory_app.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-     val signUpBtn = findViewById<Button>(R.id.RegisterBtn)
-        signUpBtn.setOnClickListener{
-            val intent = Intent(this,SignUpActivity::class.java)
-            startActivity(intent)
+        val signUpBtn = findViewById<Button>(R.id.RegisterBtn)
+            signUpBtn.setOnClickListener{
+                val intent = Intent(this,SignUpActivity::class.java)
+                startActivity(intent)
         }
-        val logInBtn = findViewById<Button>(R.id.LoginBtn)
-        logInBtn.setOnClickListener{
-            val intent = Intent(this,DashboardActivity::class.java)
-            startActivity(intent)
+//        val logInBtn = findViewById<Button>(R.id.LoginBtn)
+//        logInBtn.setOnClickListener{
+//            val intent = Intent(this,DashboardActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
+        binding.LoginBtn.setOnClickListener{
+            val email = binding.Email.text.toString()
+            val password = binding.Password.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword( email,password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, DashboardActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText( this, "Please fill all the fields!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/inventory_app/SignUpActivity.kt
+++ app/src/main/java/com/example/inventory_app/SignUpActivity.kt
@@ -1,20 +1,66 @@
 package com.example.inventory_app
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.inventory_app.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.jar.Attributes.Name
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val registerBtn = findViewById<Button>(R.id.SignUpBtn)
-        registerBtn.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
+//        This line of code directly move to login page when SignUpBtn is clicked
+//        val registerBtn = findViewById<Button>(R.id.SignUpBtn)
+//        registerBtn.setOnClickListener{
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        binding.signIn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-    }
 
+        binding.SignUpBtn.setOnClickListener{
+            val name = binding.NameSignUp.text.toString()
+            val email = binding.EmailSignUp.text.toString()
+            val password = binding.PasswordSignUp.text.toString()
+            val confirmpass = binding.ConfirmPassword.text.toString()
+
+            if(name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmpass.isNotEmpty()) {
+                if (password == confirmpass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText( this, "Password do not match!", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText( this, "Please fill empty fields!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
